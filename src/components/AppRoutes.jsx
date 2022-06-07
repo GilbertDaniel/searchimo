50b52778e391e4794a0daebdf591d23ab3d2c35d
GilbertDaniel <daniel.gilbert247@gmail.com>
2022-06-07 15:18:22 +0630

    Implementing Routes

--- src/components/AppRoutes.jsx
+++ src/components/AppRoutes.jsx
@@ -1,7 +1,17 @@
 import React from 'react'
+import { Link, Route, Routes, Navigate } from 'react-router-dom'
+import { Results } from './Results'
 
 export const AppRoutes = () => {
   return (
-    <div>App</div>
+    <div className="p-4">
+    <Routes>
+      <Route exact path="/" element={<Navigate to="/search"/>}></Route>
+      <Route exact path="/search" element={<Results/>}></Route>
+      <Route exact path="/images" element={<Results/>}></Route>
+      <Route exact path="/news" element={<Results/>}></Route>
+      <Route exact path="/videos" element={<Results/>}></Route>
+    </Routes>
+  </div>
   )
 }
