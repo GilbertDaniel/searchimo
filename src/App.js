cf447f995dda7a6a43e4ff0a6310a6bcc9dcf937
GilbertDaniel <daniel.gilbert247@gmail.com>
2022-06-07 15:03:00 +0630

    Implementing Navbar layout

--- src/App.js
+++ src/App.js
@@ -4,16 +4,18 @@ import { Header } from './components/Header';
 import { Footer } from './components/Footer';
 import { AppRoutes } from './components/AppRoutes';
 const App = () => {
-    const [darkTheme, setDarkTheme] = useState(false)
-  return (
-    <div className={darkTheme ? 'dark' : ''}>
-        <div className='bg-gray-100 dark:bg-gray-900 dark:text-gray-200 min-h-screen'>
-            <Navbar/>
-            <AppRoutes/>
-            <Footer/>
+    const [darkTheme, setDarkTheme] = useState(false);
+  
+    return (
+      <div className={darkTheme ? 'dark' : ''}>
+        <div className="dark:bg-gray-900 bg-gray-100 dark:text-gray-200 black min-h-screen">
+          <Navbar setDarkTheme={setDarkTheme} darkTheme={darkTheme} />
+          <AppRoutes />
+          <Footer />
         </div>
-    </div>
-  )
-}
+      </div>
+  
+    );
+  };
 
 export default App
